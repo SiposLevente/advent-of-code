baf77c61946d548d1a484a4ac934ad1250a643eb
SiposLevente <sipi.levente@gmail.com>
2022-02-12 11:12:40 +0100

    added puzzle input form file

--- 2021/day-06/day-6-v2/src/main.rs
+++ 2021/day-06/day-6-v2/src/main.rs
@@ -1,7 +1,10 @@
+use std::{fs};
+
 fn main() {
     let days_to_observe = 80;
-    let mut school = vec![3, 4, 3, 1, 2];
-    println!("Initial day:\t{:?}", school);
+    //let days_to_observe = 256;
+    let mut school = read_puzzle("puzzle.txt");
+    //println!("Initial day:\t{:?}", school);
 
     for i in 1..=days_to_observe {
         for fish in 0..school.len() {
@@ -12,11 +15,11 @@ fn main() {
             }
         }
 
-        if i == 1 {
-            println!("After {} day:\t{:?}", i, school);
-        } else {
-            println!("After {} days:\t{:?}", i, school);
-        }
+        // if i == 1 {
+        //     println!("After {} day:\t{:?}", i, school);
+        // } else {
+        //     println!("After {} days:\t{:?}", i, school);
+        // }
     }
 
     println!(
@@ -24,4 +27,22 @@ fn main() {
         days_to_observe,
         school.len()
     );
+}
+
+fn read_puzzle(file_path: &str) -> Vec<isize> {
+    let data = if let Ok(i) = fs::read_to_string(file_path) {
+        i
+    } else {
+        panic!("Error reading file!");
+    };
+
+    if let Some(i) = data.lines().next() {
+        i.split(",")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|x| x.trim().parse().unwrap())
+            .collect()
+    } else {
+        panic!("Invalid input!");
+    }
 }
